3750e68b095d70e09120ec10d9bc283f75890e02
ismael <leamsigc@hotmail.com>
2018-01-23 09:07:10 -0500

    add auto prefixer

--- projects/NexusRideHome/dist/c83b962d76bf230f1725ba1002cd66e9.js
+++ projects/NexusRideHome/dist/c83b962d76bf230f1725ba1002cd66e9.js
@@ -167,7 +167,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
-  var ws = new WebSocket('ws://' + window.location.hostname + ':52353/');
+  var ws = new WebSocket('ws://' + window.location.hostname + ':58860/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
