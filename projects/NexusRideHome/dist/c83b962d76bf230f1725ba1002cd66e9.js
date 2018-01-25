908acdd14be1bb3fb4b09465fc2c9780b1cf8a28
ismael <leamsigc@hotmail.com>
2018-01-25 16:33:40 -0500

    change the get stimates form

--- projects/NexusRideHome/dist/c83b962d76bf230f1725ba1002cd66e9.js
+++ projects/NexusRideHome/dist/c83b962d76bf230f1725ba1002cd66e9.js
@@ -69,7 +69,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({14:[function(require,module,exports) {
+})({17:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -100,7 +100,7 @@ function getBaseURL(url) {
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
 
-},{}],12:[function(require,module,exports) {
+},{}],15:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,13 +132,13 @@ function reloadCSS() {
 
 module.exports = reloadCSS;
 
-},{"./bundle-url":14}],11:[function(require,module,exports) {
+},{"./bundle-url":17}],14:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./..\\img\\herobg.jpeg":["b7a6ed50c210d83ac864aa7c3a7e57d3.jpeg",13],"_css_loader":12}],10:[function(require,module,exports) {
+},{"./..\\img\\herobg.jpeg":["b7a6ed50c210d83ac864aa7c3a7e57d3.jpeg",16],"_css_loader":15}],10:[function(require,module,exports) {
 "use strict";
 
 require("../css/app.scss");
@@ -149,7 +149,7 @@ function toggleNav() {
 }
 // toggle menu
 document.querySelector(".toggle").addEventListener("click", toggleNav);
-},{"../css/app.scss":11}],0:[function(require,module,exports) {
+},{"../css/app.scss":14}],0:[function(require,module,exports) {
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
 function Module() {
@@ -167,7 +167,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
-  var ws = new WebSocket('ws://' + window.location.hostname + ':50685/');
+  var ws = new WebSocket('ws://' + window.location.hostname + ':64673/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
