41e60cd0a58673587914db60bdc6f97606a8b883
leamsigc <leamsigc@hotmail.com>
2018-02-11 09:17:22 -0500

    loader with css variables

---
+++ Experiments/loader/app.js
@@ -0,0 +1,16 @@
+// windo load
+window.addEventListener('load', () => {
+  const input = document.querySelector('.input');
+  //input change value
+  //Get the var value
+  /*
+  const style = getComputedStyle(document.documentElement);
+  const colorValue  =  style.getPropertyValue('--main-bg');
+  */
+  input.addEventListener('change', _ => {
+    //set the var value
+    document.documentElement.style.setProperty('--main-bg',_.target.value);
+
+
+  });
+} );
---
+++ Experiments/readme.md
@@ -0,0 +1,2 @@
+# Experiment
+* Simple loader 
